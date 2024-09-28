50d0ae3419bfa1c0ad9fce8416e5cdc53f3eb74e
unknown <krishmoorjani04@gmail.com>
2024-09-28 22:26:48 +0530

    diet plan endpoint

--- backend/controllers/planController.js
+++ backend/controllers/planController.js
@@ -1,10 +1,21 @@
 import { createResponse } from '../utils/openai.js'; 
 import { generatePrompt } from '../utils/prompt.js';
+import supabase from '../supabaseClient.js'
 
 export const createPlanController = async (req, res) => {
     const { weight, height, gender, weightloss, dietpreference, user_id, age, goal, activitylevel, calorieintake, duration } = req.body;
     const prompt = generatePrompt(weight, height, gender, weightloss, dietpreference, user_id, age, goal, activitylevel, calorieintake, duration)
     const plan = await createResponse(prompt)
     console.log(plan)
+
+    const { data: insert_data, error: insert_error } = await supabase
+        .from('diet_plan')
+        .insert([{ 
+            user_id, plan
+        }]);
+
+        console.log(insert_data, insert_error)
+    res.status(201).json({ message: 'User Log registered successfully'});
+
     return res.send(plan)
 };
