9e72a2d46613da74c83c9b03bf7174c3bebe5f8c
Vinitthakkar45 <45vinitthakkar@gmail.com>
2024-09-28 22:55:11 +0530

    fix add user

--- backend/controllers/planController.js
+++ backend/controllers/planController.js
@@ -4,22 +4,18 @@ import supabase from '../supabaseClient.js'
 
 export const createPlanController = async (req, res) => {
     const { weight, height, gender, dietpreference, user_id, age, goal, activitylevel, duration } = req.body;
-    // const prompt = generatePrompt(weight, height, gender, dietpreference, user_id, age, goal, activitylevel, duration)
-    // const plan = await createResponse(prompt)
-    // console.log(plan)
+    const prompt = generatePrompt(weight, height, gender, dietpreference, user_id, age, goal, activitylevel, duration)
+    const plan = await createResponse(prompt)
+    console.log(plan)
 
-    /*
-
-    */
-    plan="abcdef"
+    const data="abcdef"
     const { data: insert_data, error: insert_error } = await supabase
+        
         .from('diet_plan')
-        .insert([{ 
-            user_id, plan
-        }]);
+        .insert({ 
+            user_id,data
+        });
 
         console.log(insert_data, insert_error)
-    res.status(201).json({ message: 'User Log registered successfully'});
-
-    return res.send(plan)
+        res.status(201).json({ message: 'User Log registered successfully'});
 };
