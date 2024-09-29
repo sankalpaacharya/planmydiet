53e7532bfb0d420e35fd64c3154d28f5f6c1203e
sankalpaacharya <sankalpaacharya01@gmail.com>
2024-09-29 18:06:08 +0530

    view plan

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage";
 import StartPage from "./pages/startpage";
 import PlanPage from "./pages/planpage";
+import ViewPlanPage from "./pages/viewplan";
 function App() {
   return (
     <div>
@@ -10,6 +11,7 @@ function App() {
           <Route path="/home" element={<HomePage />}></Route>
           <Route path="/" element={<StartPage />}></Route>
           <Route path="/plan/:id" element={<PlanPage />}></Route>
+          <Route path="/view/:id" element={<ViewPlanPage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/pages/viewplan.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/navbar";
+
+export default function viewplan() {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="max-w-[90%] w-[80%] md:px-5 lg:px-0 mt-5 ">
+        <Navbar />
+      </div>
+    </div>
+  );
+}
