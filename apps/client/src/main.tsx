20cab0aa78ffeef3b7a49a0eca9513c1add8b721
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-01-19 14:44:52 +0530

    chore(setup): shadcn setup

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <ThemeProvider
+    attribute="class"
+    defaultTheme="dark"
+    disableTransitionOnChange
+  >
     <App />
-  </StrictMode>,
-)
+  </ThemeProvider>
+);
