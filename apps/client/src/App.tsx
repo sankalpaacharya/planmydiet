1046e4298303bb35d6fdcfeae9becc374a5f703b
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-01-19 15:11:18 +0530

    feat(add): add shadcn button

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,11 +1,8 @@
 import "./App.css";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return (
-    <>
-      <div>hello world</div>
-    </>
-  );
+  return <HomePage />;
 }
 
 export default App;
---
+++ apps/client/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+type Props = {};
+import { Button } from "@/components/ui/button";
+
+export default function HomePage({}: Props) {
+  return (
+    <div className="p-6">
+      <Button>Click me Daddy</Button>
+    </div>
+  );
+}
