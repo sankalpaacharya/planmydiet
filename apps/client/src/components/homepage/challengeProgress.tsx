3214cbd9751c6261c15c3f3401719123d0e12593
Vinitthakkar45 <45vinitthakkar@gmail.com>
2025-01-20 00:19:10 +0530

    (refactor): change space, color theme in challenge Progress

--- apps/client/src/components/homepage/challengeProgress.tsx
+++ apps/client/src/components/homepage/challengeProgress.tsx
@@ -20,45 +20,41 @@ const ChallengeProgress = ({ planDuration, currDay }: ChallengeProps) => {
   const currIndex = days.findIndex((day) => day === curDay);
 
   return (
-    <main className="flex">
-      <Card className="w-72">
-        <CardHeader>
-          <CardTitle className="text-xl font-normal text-center">
-            Challenge Progress
-          </CardTitle>
-        </CardHeader>
-        <CardContent className="flex flex-col items-center gap-4">
-          <p className="text-2xl font-bold">Day {currDay}</p>
-          <Progress value={55} className="w-full" />
-          <p className="text-sm text-gray-500">
-            {planDuration - currDay} days remaining
-          </p>
-        </CardContent>
-        <CardFooter className="flex justify-around gap-2 w-full">
-          {days.map((day, index) => (
+    <Card className="w-72">
+      <CardHeader>
+        <CardTitle className="text-xl font-normal text-center">
+          Challenge Progress
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col items-center gap-2">
+        <p className="text-2xl font-bold">Day {currDay}</p>
+        <Progress value={60} className="w-full" />
+        <p className="text-sm text-gray-500">
+          {planDuration - currDay} days remaining
+        </p>
+      </CardContent>
+      <CardFooter className="flex justify-around gap-2 w-full">
+        {days.map((day, index) => (
+          <div
+            key={index}
+            className={`flex flex-col items-center gap-2 ${
+              index > currIndex ? "font-normal text-gray-500" : "text-white-600"
+            }`}
+          >
+            <span className="text-sm">{day}</span>
             <div
-              key={index}
-              className={`flex flex-col items-center gap-2 ${
-                index >= currIndex
-                  ? "font-normal text-white-600"
-                  : "text-gray-600"
+              className={`w-4 h-4 rounded-full ${
+                index < currIndex
+                  ? "bg-rose-600" //prevoius days
+                  : index === currIndex
+                  ? "bg-rose-300 animate-pulse" //current day as blue
+                  : "border-2 bg-gray-500" // next day
               }`}
-            >
-              <span className="text-sm">{day}</span>
-              <div
-                className={`w-4 h-4 rounded-full ${
-                  index < currIndex
-                    ? "bg-green-500" //prevoius days
-                    : index === currIndex
-                    ? "bg-blue-500 animate-pulse" //current day as blue
-                    : "border-2 border-gray-300" // next day
-                }`}
-              ></div>
-            </div>
-          ))}
-        </CardFooter>
-      </Card>
-    </main>
+            ></div>
+          </div>
+        ))}
+      </CardFooter>
+    </Card>
   );
 };
 
