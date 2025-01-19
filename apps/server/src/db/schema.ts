90e3b57f6ed2cab106315b32bf539771e101c6d5
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-01-19 16:20:17 +0530

    feat(add): drizzle schema, npmrc

--- apps/server/src/db/schema.ts
+++ apps/server/src/db/schema.ts
@@ -1,8 +1,34 @@
 import { pgTable } from "drizzle-orm/pg-core"
-import {integer} from "drizzle-orm/pg-core"
+import {integer,text,uuid} from "drizzle-orm/pg-core"
+
+export const usersTable = pgTable('users_table', {
+    id: uuid().defaultRandom().primaryKey(),
+    clerkUserId:text().notNull().unique(),
+    name: text().notNull(),
+    email: text().notNull().unique(),
+  });
 
 
-export const userTable = pgTable("user_measurement",{
+export const userMeasurement = pgTable("user_measurement",{
+    userId:text().notNull(),
+    height:integer().notNull(),
+    age:integer().notNull(),
+    bfp:integer().notNull(), // body fat percentage
 })
+
+// single challenge can have the multiple meal plan, it has to be according to user not everyone inside 
+// challenge will have same plan 
+// when user is joining the challange ask them if they want to copy the meal plan or wanna make their own new 
+// goal has to be same but meal can be different
+export const challenge = pgTable("challenge",{
+    id: uuid().defaultRandom().primaryKey(),
+    title:text().notNull(),
+})
+
+
+
+
+
+
 
 
