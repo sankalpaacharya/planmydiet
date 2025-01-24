1129a60693341c18b573c72d8b930ce921199d5d
Vinitthakkar45 <45vinitthakkar@gmail.com>
2025-01-24 16:48:41 +0530

    feat(viewplan) : add top cards for veiw my plan page

--- apps/client/src/routes/plan/$id.tsx
+++ apps/client/src/routes/plan/$id.tsx
@@ -1,10 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
+import DietPlan from "@/pages/DietPlan"
 
 export const Route = createFileRoute("/plan/$id")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const { id } = Route.useParams();
-  return <div>Hello, {id}</div>;
+  // const { id } = Route.useParams();
+  return (
+    <DietPlan />
+  );
 }
