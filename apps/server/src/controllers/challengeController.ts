4a9a519f8b832625539470c769dfeb5cfe6fdc83
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-01-25 16:21:11 +0530

    feat(route): finished create challenge

--- apps/server/src/controllers/challengeController.ts
+++ apps/server/src/controllers/challengeController.ts
@@ -1,18 +1,27 @@
 import { Request,Response } from "express"
-import { insertChallengeSchema } from "../db/schema"
+import { insertChallengeSchema,insertChallengeParticipantSchema} from "../db/schema"
 import { ZodError } from "zod"
 import { zodErrorParser } from "../utils/zodErrorParser"
-
+import { insertChallenge,insertChallengeParticipant } from "../db/queries/insert"
 
 export const createChallengeController = async (req:Request,res:Response):Promise<any>=>{
     try {
-        const parse = insertChallengeSchema.parse(req.body)
-        console.log(req.auth.userId)
+
+        const userId = req.auth.userId; // way of getting the userID
+        const challengeData = insertChallengeSchema.parse({...req.body,creatorId:userId})
+        const {challengeId} = (await insertChallenge(challengeData))[0]
+        const challengeParticipant = {
+            ...req.body,
+            userId,
+            challengeId
+        }
+        const challengeParticipantData = insertChallengeParticipantSchema.parse(challengeParticipant)
+        await insertChallengeParticipant(challengeParticipantData)
         res.send({success:"data is parsed sucessfully"})
     } catch (error) {
         if(error instanceof ZodError){
-            res.send({errors:zodErrorParser(error)})
+            return res.send({errors:zodErrorParser(error)})
         }
+        res.send({error:"oops some error has occured"})
     }
-    
 }
