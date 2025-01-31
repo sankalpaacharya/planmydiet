91b8f7a827fee3bf42942eb94171bc1561f06a66
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-01-31 18:11:03 +0530

    feat(meallog): diet card change

--- apps/client/src/pages/DietLayout.tsx
+++ apps/client/src/pages/DietLayout.tsx
@@ -1,8 +1,14 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import { useEffect, useState } from "react";
 import { Link } from "@tanstack/react-router";
 import { api } from "@/lib/axios";
-import { Salad, CircleDollarSign , BicepsFlexed, UtensilsCrossed, ShieldPlus } from "lucide-react";
+import {
+  Salad,
+  CircleDollarSign,
+  BicepsFlexed,
+  UtensilsCrossed,
+  ShieldPlus,
+} from "lucide-react";
 
 interface SelectPlan {
   goal: string;
@@ -44,7 +50,6 @@ export default function DietPlanPage() {
             mealPlans.map((meal: any, index) => (
               <Link
                 href={`/plan/${meal.id}`}
-<<<<<<< HEAD
                 to={`/plan/$id`}
                 key={index}
                 params={{ id: meal.id }}
@@ -76,55 +81,6 @@ export default function DietPlanPage() {
                     </p>
                     <p className="text-lg w-2/3">{meal.budget}</p>
                   </div>
-=======
-                to={`/plan/${meal.id}`}
-                key={index}>
-                <Card className="max-w-md w-full">
-                  <CardHeader className="space-y-1">
-                    <div className="flex items-center justify-between">
-                      <h2 className="text-2xl font-bold ">
-                        {meal.goal}
-                      </h2>
-                    </div>
-                  </CardHeader>
-                  <CardContent className="space-y-4">
-                    <div className="flex items-start gap-2">
-                      <Salad className="w-5 h-5 text-blue-500 mt-1" />
-                      <div className="flex gap-3">
-                        <span className="font-semibold text-gray-400">
-                          Diet Prefrence : {meal.dietPreference}
-                        </span>
-                        <p className="text-gray-400"></p>
-                      </div>
-                    </div>
-                    <div className="flex items-start gap-2">
-                      <CircleDollarSign  className="w-5 h-5 text-green-500 mt-1" />
-                      <div>
-                        <h3 className="font-semibold text-gray-400">
-                          Budget : {meal.budget}
-                        </h3>
-                      </div>
-                    </div>
-                    <div className="flex items-center gap-2 text-gray-400">
-                      <BicepsFlexed  className="w-5 h-5 text-orange-500" />
-                      <span className="font-medium">
-                        Activity Level : {meal.activityLevel}
-                      </span>
-                    </div>
-                    <div className="flex items-center gap-2 text-gray-400">
-                      <UtensilsCrossed   className="w-5 h-5 text-amber-300" />
-                      <span className="font-medium">
-                        Calorie Intake Limit : {meal.calorieIntake}
-                      </span>
-                    </div>
-                    <div className="flex items-center gap-2 text-gray-400">
-                      <ShieldPlus   className="w-5 h-5 text-teal-600" />
-                      <span className="font-medium">
-                        Medical Conditions : {meal.medicalConditions}
-                      </span>
-                    </div>
-                  </CardContent>
->>>>>>> 61b874bc24c7f69c81bbc3fda8812fb2582cb4c6
                 </Card>
               </Link>
             ))
