5e228a5d41d163f0ec5f09b6139e2fb7877feda1
Yatri Gor <yatrigor29@gmail.com>
2025-01-31 16:20:37 +0530

    Final Commit

--- apps/client/src/pages/DietLayout.tsx
+++ apps/client/src/pages/DietLayout.tsx
@@ -32,33 +32,43 @@ export default function DietPlanPage() {
   }, []);
 
   return (
-    <div className="w-full p-4 mb-16 flex justify-center">
-      {loading ? (
-        <p>Loading meal plans...</p>
-      ) : error ? (
-        <p className="text-red-500">{error}</p>
-      ) : (
-        <div className="w-full max-w-7xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {mealPlans.length > 0 ? (
-            mealPlans.map((meal: any, index) => (
-              <Link
-                href={`/plan/${meal.id}`}
-                to={`/plan/$id`}
-                params={{ id: meal.id }}
-              >
-                <Card key={index} className="flex flex-col p-4">
-                  <h3 className="text-xl font-semibold">Goal: {meal.goal}</h3>
-                  <p>Diet Preference: {meal.dietPreference}</p>
-                  <p>Calorie Intake: {meal.calorieIntake} calories</p>
-                  <p>Budget: {meal.budget}</p>
-                </Card>
-              </Link>
-            ))
-          ) : (
-            <p>No meal plans available.</p>
-          )}
-        </div>
-      )}
-    </div>
+    <div className="w-full p-4 pl-0 mb-16 flex justify-center">
+    {loading ? (
+      <p>Loading meal plans...</p>
+    ) : error ? (
+      <p className="text-rose-600">{error}</p>
+    ) : (
+      <div className="w-full max-w-7xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {mealPlans.length > 0 ? (
+          mealPlans.map((meal: any, index) => (
+            <Link href={`/plan/${meal.id}`} to={`/plan/${meal.id}`} key={index}>
+              <Card className="flex flex-col p-4 shadow-md rounded-lg hover:shadow-xl transition-shadow duration-300">
+                
+                <h3 className="text-2xl font-semibold italic text-rose-400 mb-3">{meal.goal}</h3>
+
+                <div className="flex mb-3 border-b">
+                  <p className="text-sm font-medium w-1/3 text-gray-400">Diet Preference:</p>
+                  <p className="text-lg w-2/3">{meal.dietPreference}</p>
+                </div>
+                
+                <div className="flex mb-3 border-b">
+                  <p className="text-sm text-gray-400 font-medium w-1/3">Calorie Intake:</p>
+                  <p className="text-lg w-2/3">{meal.calorieIntake} calories</p>
+                </div>
+  
+                <div className="flex border-b">
+                  <p className="text-sm font-medium w-1/3 text-gray-400">Budget:</p>
+                  <p className="text-lg w-2/3">{meal.budget}</p>
+                </div>
+              </Card>
+            </Link>
+          ))
+        ) : (
+          <p>No meal plans available.</p>
+        )}
+      </div>
+    )}
+  </div>
+  
   );
 }
